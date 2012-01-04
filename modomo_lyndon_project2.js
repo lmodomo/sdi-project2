9370d4c4c1904db488f7093e09bf9b8d51f0b8aa
Lyndon Modomo <lmodomo@fullsail.com>
2012-01-04 11:44:06 -0500

    Final Commit on Project2

--- modomo_lyndon_project2.js
+++ modomo_lyndon_project2.js
@@ -6,10 +6,11 @@
 var niceBeachDay = true,
 	sunnyDay = true,
 	surfsUp = true,
-	numOfWavesInSet = 6,
 	goodSurfDay,
+	numOfWavesInSet = 6,
 	waveCnt,
-	surfers = ["surfer1", "surfer2", "surfer3"],
+	remainingWaveCnt,
+	surfers = ["Surfer1", "Surfer2", "Surfer3", "Surfer4"],
 	slang1 = "Surf\'s Up Brah",
 	slang2 = "Goofy footed",
 	surferTalk
@@ -19,20 +20,20 @@ var niceBeachDay = true,
 // (Procedure Task): Check to see if it is a good day for the beach or not
 var goodDayForBeach = function (goodDay) { 
 	if (goodDay === true) {   
-		console.log ("It is a great day for the beach!!");
+		console.log ("(Procedure Task): It is a great day for the beach!!");
 	}else {
-		console.log ("It is NOT a great day for the beach, bummer dude...");
+		console.log ("(Procedure Task): It is NOT a great day for the beach, bummer dude...");
 	}
 };  //end goodDayForBeach function
 
 
 // (Boolean Function Task): Check to see if it will be a sunny day and check to see if "Surf's Up!"
 var idealDayToSurf = function (sunny, niceWaves) { 
-	if (sunny === niceWaves) {   
-		console.log ("It is a great day to surf because the sun is out and Surf's Up Brah!!");
+	if (sunny && niceWaves) {   
+		console.log ("(Boolean Function Task): It is a great day to surf because the sun is out and Surf's Up Brah!!");
 		return (true);
 	}else {     
-		console.log ("It is NOT a great day to surf.");
+		console.log ("(Boolean Function Task): It is NOT a great day to surf.");
 		return (false);
 	}
 };  //end idealDayToSurf function
@@ -45,6 +46,7 @@ var sets = function (continuousWaves) {
 		i = 1,
 		numOfWaves = 0;
 	
+	console.log ("(Number Function Task) Output From The While Loop Listed Below:");
 	while (i <= wavesInSet) {
 		numOfWaves++;
 		console.log ("Wave number " + i + " in the set of waves just passed by.");
@@ -58,6 +60,7 @@ var sets = function (continuousWaves) {
 var catchWave = function (wavesInSet, surfer) { 
 	var wavesLeft = wavesInSet;
 	
+	console.log ("(Array Function Task): Output From the FOR Loop Listed Below:");
 	for (var i = 0, j = surfer.length; i < j; i++) {
 		wavesLeft--;
 		console.log ("Wave number " + (i+1) + " in the set.  " + surfer[i] + " will catch this wave.");
@@ -65,11 +68,11 @@ var catchWave = function (wavesInSet, surfer) {
 	return (wavesLeft);	
 };  //end catchWave function
 
-//(String Function Task): Surfers have a strange lingo, where are some concatenated slang words.
+//(String Function Task) Concatenated slang words.
 var slangVerse = function (phase1, phase2) { 
 	var phase;
 	
-	phase = "Why do surfer's have such a strange language.  They say things like " + phase1 + ", " + phase2 + ", etc...";
+	phase = "(String Function Task /Returned Value): Why do surfer's have such a strange language.  They say things like " + phase1 + ", " + phase2 + ", etc...";
 	return (phase);	
 };  //end slangVerse function
 
@@ -79,15 +82,15 @@ goodDayForBeach (niceBeachDay);
 
 //For Boolean Function Task
 goodSurfDay = idealDayToSurf (sunnyDay, surfsUp);
-console.log ("Is it a good day to surf : ", goodSurfDay);
+console.log ("(Boolean Function Task / Returned Value):Is it a good day to surf : ", goodSurfDay);
 
 //For Number Function Task
 waveCnt = sets(numOfWavesInSet);
-console.log ("The numbers of waves in a set: ", waveCnt);
+console.log ("(Number Number Task / Returned Value): The numbers of waves in a set: ", waveCnt);
 
 //For Array Function Task
-waveCnt = catchWave(numOfWavesInSet, surfers);
-console.log ("The numbers of waves remaining in the set: ", waveCnt);
+remainingWaveCnt = catchWave(waveCnt, surfers);   //passing the previous returned value into this requirement
+console.log ("(Array Function Task / Returned Value):The numbers of waves remaining in the set: ", remainingWaveCnt);
 
 //For String Function Task
 surferTalk = slangVerse(slang1, slang2);
